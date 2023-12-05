d02256dfbb6dcb0b3541850f47ee4bce2fde2849
Vincent Moeykens <vincent@beta.team>
2023-12-05 11:38:30 -0500

    Adds input and structure for day5

---
+++ day5/day5.py
@@ -0,0 +1,17 @@
+def part1(input_file='input'):
+    pass
+
+
+def part2(input_file='input'):
+    pass
+
+
+def main():
+    print(f'part1 test: {part1("test_input")}')
+    print(f'part1: {part1()}')
+    print(f'part2 test: {part2("test_input")}')
+    print(f'part2: {part2()}')
+
+
+if __name__ == '__main__':
+    main()
